379a0436b0fa4da3a01e85b642a278e8e0b5e1c5
CodingForFunAndProfit <mail@florianstolz.net>
2019-01-24 00:19:27 +0100

    + winston logger with rotating log-files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 var nunjucks = require('nunjucks');
+var winston = require("./config/winston-config");
+var fs = require("fs");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +17,9 @@ nunjucks.configure('views', {
 });
 app.set('view engine', 'njk');
 
-app.use(logger('dev'));
+
+
+app.use(morgan("combined", { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -23,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 module.exports = app;
