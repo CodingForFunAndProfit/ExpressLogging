b25258e2346387b5ca2285e8f092c3391ff53a54
CodingForFunAndProfit <mail@florianstolz.net>
2019-01-23 15:05:08 +0100

    added nunjucks, index view, gitignore

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var nunjucks = require('nunjucks');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express   : app
+});
+app.set('view engine', 'njk');
 
 app.use(logger('dev'));
 app.use(express.json());
